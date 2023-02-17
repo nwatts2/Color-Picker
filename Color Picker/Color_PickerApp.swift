de4f37c4474519696dc2e58e5639f40c06f18de2
Noah Watts <nwatts2@uccs.edu>
2023-02-17 09:05:46 -0700

    Initial Commit

---
+++ Color Picker/Color_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_PickerApp.swift
+//  Color Picker
+//
+//  Created by Nuah on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Color_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
